897ada9ab77efdd55bdf3731a74c82d2616fa66e
shohei1029 <shohei@sfc.keio.ac.jp>
2020-12-29 23:42:42 +0900

    add IMEON/IMEOFF macro

--- mint60/sn02/keymap.c
+++ mint60/sn02/keymap.c
@@ -27,7 +27,9 @@ extern rgblight_config_t rgblight_config;
 #endif
 
 enum custom_keycodes {
-  RGBRST = SAFE_RANGE
+  RGBRST = SAFE_RANGE,
+  IMEON,
+  IMEOFF
 };
 
 
@@ -37,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
-    KC_ESC,    KC_LALT,    KC_LGUI,    KC_LANG2(KC_MHEN),    KC_SPC,        KC_BSPC, KC_LANG1(KC_HENK),  KC_RGUI,               KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_ESC,    KC_LALT,    KC_LGUI,    IMEOFF,    KC_SPC,        KC_BSPC, IMEON,  KC_RGUI,               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
@@ -79,6 +81,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       #endif
       break;
+    case IMEON:
+       if (record->event.pressed) {
+         register_code(KC_HENK);
+         register_code(KC_LANG1);
+       } else {
+         unregister_code(KC_HENK);
+         unregister_code(KC_LANG1);
+       }
+       break;
+    case IMEOFF:
+       if (record->event.pressed) {
+         register_code(KC_MHEN);
+         register_code(KC_LANG2);
+       } else {
+         unregister_code(KC_MHEN);
+         unregister_code(KC_LANG2);
+       }
+       break;
   }
   return true;
 }
